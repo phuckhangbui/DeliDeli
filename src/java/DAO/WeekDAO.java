2d3d12badcf51c2f9010190d84f403ee543ef8f9
WalkingBag <OfficialAyashiro@gmail.com>
2023-07-27 15:22:11 +0700

    Adding deleteWeek for weekDAO

--- src/java/DAO/WeekDAO.java
+++ src/java/DAO/WeekDAO.java
@@ -104,4 +104,42 @@ public class WeekDAO {
         }
         return result;
     }
+    
+    public static boolean deleteWeekOnId(int weekId){
+        Connection con = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+
+        String sql = "DELETE d\n"
+                + "FROM [Week] d\n"
+                + "WHERE d.id = ?";
+
+        try {
+            con = DBUtils.getConnection();
+            if (con != null) {
+                stm = con.prepareStatement(sql);
+                stm.setInt(1, weekId);
+
+                int rowsAffected = stm.executeUpdate();
+                return rowsAffected > 0;
+            }
+        } catch (SQLException ex) {
+            System.out.println("Query error - deleteDateByPlanId: " + ex.getMessage());
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stm != null) {
+                    stm.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println("Error closing database resources: " + ex.getMessage());
+            }
+        }
+        return false;
+    }
 }
